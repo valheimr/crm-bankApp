c4803110c4fc94dfe4958b32e741e74bd83d78ae
Alex <ilves.aleksei@mail.ru>
2020-05-20 20:10:31 +0300

    Installed Vuelidate and added login validation + some messages with type-in errors

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -8,6 +9,8 @@ import store from './store'
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuelidate)
 
 Vue.filter('date', dateFilter)
 
